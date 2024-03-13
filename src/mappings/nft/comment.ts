ccb3a807d00bce2edcbeeb6e6fc15204114dfa96
Arsen Lulukian <85998249+basya01@users.noreply.github.com>
2024-03-13 15:07:00 +0200

    feat: add sectionId

--- src/mappings/nft/comment.ts
+++ src/mappings/nft/comment.ts
@@ -1,7 +1,8 @@
-import { store } from '@graphprotocol/graph-ts'
+import { store, json, log, JSONValue } from '@graphprotocol/graph-ts'
 import { CommentRemoved, Commented } from '../../types/templates/NFT/SX1155NFT'
 import { Comment } from '../../wrappers/comment'
 import { Token } from '../../wrappers/nft-token'
+import { jsonUtils } from '../../utils/json'
 
 export function handleComment(event: Commented): void {
   const nftAddress = event.address
@@ -10,6 +11,30 @@ export function handleComment(event: Commented): void {
 
   const tokenCreatedId = Token.buildID(nftAddress, tokenId)
   const comment = new Comment(nftAddress, tokenId, commentId)
+
+  const commentJsonValue = json.fromString(event.params.comment)
+
+  if (commentJsonValue.isNull()) {
+    log.warning('WARNING: Failed to parse json from string', [])
+    return
+  }
+
+  const commentData = commentJsonValue.toObject()
+
+  if (commentData.entries.length === 0) {
+    log.warning('WARNING: JSON is empty tokenId {}', [tokenCreatedId])
+    return
+  }
+
+  const jsonSectionId = changetype<JSONValue | null>(
+    commentData.get('sectionId'),
+  )
+  const sectionId = jsonUtils.parseString(jsonSectionId)
+
+  if (sectionId && isNullable(sectionId) === false) {
+    comment.sectionId = sectionId
+  }
+
   comment.uri = event.params.comment
   comment.commentator = event.params.commentator
   comment.createdAt = event.block.timestamp
