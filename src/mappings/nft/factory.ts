8c320d01f472aa25118a0472164cf5bb77ed511f
Arsen Lulukian <arsen@swarm.com>
2024-04-25 23:10:08 +0300

    add setUriJson method to NFT class

--- src/mappings/nft/factory.ts
+++ src/mappings/nft/factory.ts
@@ -1,7 +1,6 @@
-import { Address, Bytes, dataSource, json, log } from '@graphprotocol/graph-ts'
+import { Address, Bytes, dataSource } from '@graphprotocol/graph-ts'
 import { SX1155NFTDeployed } from '../../types/NFTFactory/SX1155NFTFactory'
 import { NFT, NFTFactory } from '../../wrappers'
-import { jsonUtils } from '../../utils/json'
 
 export function handleCreateNFT(event: SX1155NFTDeployed): void {
   let factoryAddress = dataSource.address().toHex()
@@ -12,40 +11,10 @@ export function handleCreateNFT(event: SX1155NFTDeployed): void {
   let address = changetype<Address>(event.params.deployedAddress)
   let nft = new NFT(address)
 
-  const nftJsonValue = json.try_fromString(event.params.uri)
+  nft.setUriJson(event.params.uri)
 
-  if (nftJsonValue.isError) {
-    log.warning('WARNING: Failed to parse json from string nftId {}', [nft.id])
-    return
-  }
-
-  const nftData = nftJsonValue.value.toObject()
-
-  if (nftData.entries.length === 0) {
-    log.warning('WARNING: JSON is empty nftId {}', [nft.id])
-    return
-  }
-
-  const jsonLogoUrl = nftData.get('logoUri')
-  const jsonIndexPagesUri = nftData.get('indexPagesUri')
-  const jsonUri = nftData.get('uri')
-
-  if (jsonLogoUrl === null || jsonIndexPagesUri === null || jsonUri === null) {
-    log.warning('WARNING: Invalid JSON format nftId {}', [nft.id])
-    return
-  }
-
-  const logoUrl = changetype<string>(jsonUtils.parseString(jsonLogoUrl))
-  const indexPagesUri = changetype<string>(
-    jsonUtils.parseString(jsonIndexPagesUri),
-  )
-  const uri = changetype<string>(jsonUtils.parseString(jsonUri))
-
-  nft.logoUrl = logoUrl
-  nft.indexPagesUri = indexPagesUri
   nft.symbol = params.symbol
   nft.name = params.name
-  nft.uri = uri
 
   const editor = Bytes.fromHexString(event.params.editor.toHex())
   const admin = Bytes.fromHexString(event.params.admin.toHex())
--- src/mappings/nft/nft.ts
+++ src/mappings/nft/nft.ts
@@ -1,69 +1,29 @@
-import { json, log, JSONValue } from '@graphprotocol/graph-ts'
 import { ContractURISet } from '../../types/NFTFactory/SX1155NFT'
 import { NFTURIUpdate } from '../../types/schema'
-import { jsonUtils } from '../../utils/json'
 import { NFT } from '../../wrappers'
 
 export function handleUpdateNFTUri(event: ContractURISet): void {
   const nft = NFT.mustLoad(event.address.toHexString())
 
-  const nftJsonValue = json.try_fromString(event.params.uri)
-
-  if (nftJsonValue.isError) {
-    log.warning('WARNING: Failed to parse json from string nftId {}', [nft.id])
-    return
-  }
-
-  const nftData = nftJsonValue.value.toObject()
-
-  if (nftData.entries.length === 0) {
-    log.warning('WARNING: JSON is empty commentId {}', [nft.id])
-    return
-  }
-
-  const jsonLogoUrl = nftData.get('logoUri')
-  const jsonIndexPagesUri = nftData.get('indexPagesUri')
-  const jsonUri = nftData.get('uri')
-  const jsonName = nftData.get('name')
+  const changedFields = nft.setUriJson(event.params.uri)
 
   if (
-    jsonLogoUrl === null &&
-    jsonIndexPagesUri === null &&
-    jsonUri === null &&
-    jsonName === null
+    changedFields !== null &&
+    changedFields.uri !== null &&
+    changedFields.previousUri !== null
   ) {
-    log.warning('WARNING: Invalid JSON format {}', [nft.id])
-    return
-  }
-
-  const logoUrl = changetype<string>(jsonUtils.parseString(jsonLogoUrl))
-  const indexPagesUri = changetype<string>(
-    jsonUtils.parseString(jsonIndexPagesUri),
-  )
-  const uri = changetype<string>(jsonUtils.parseString(jsonUri))
-  const name = changetype<string>(jsonUtils.parseString(jsonName))
-
-  if (logoUrl !== null) {
-    nft.logoUrl = logoUrl
-  }
-  if (indexPagesUri !== null) {
-    nft.indexPagesUri = indexPagesUri
-  }
-  if (name !== null) {
-    nft.name = name
-  }
-
-  if (uri !== null) {
     const updatedNFT = new NFTURIUpdate(
       event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
     )
+
+    const previousURI = changetype<string>(changedFields.previousUri)
+    const newURI = changetype<string>(changedFields.uri)
+
     updatedNFT.nft = nft.id
-    updatedNFT.previousURI = nft.uri
-    updatedNFT.newURI = uri
+    updatedNFT.previousURI = previousURI
+    updatedNFT.newURI = newURI
     updatedNFT.updatedAt = event.block.timestamp
     updatedNFT.save()
-
-    nft.uri = uri
   }
 
   nft.save()
